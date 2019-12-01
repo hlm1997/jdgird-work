a84da8bcafcf2e97717665ea1976a9a23b691a07
hlm <664735600@qq.com>
2019-12-01 15:56:12 +0800

    add sout

--- src/main/java/com/baizhi/SpringBoot.java
+++ src/main/java/com/baizhi/SpringBoot.java
@@ -9,5 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBoot {
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot.class, args);
+        System.out.println();
     }
 }
